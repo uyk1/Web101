3dd68be15094d3137e25b07c3479ec1689b6a0b9
uyk1 <uyk0928@gmail.com>
2023-01-24 12:07:18 +0900

    backend base

---
+++ demo/src/main/java/com/example/demo/service/TodoService.java
@@ -0,0 +1,20 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.TodoEntity;
+import com.example.demo.persistence.TodoRepository;
+
+@Service
+public class TodoService {
+	@Autowired
+	public TodoRepository repository;
+	
+	public String testService() {
+		TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
+		repository.save(entity);
+		TodoEntity savedEntity = repository.findById(entity.getId()).get();
+		return savedEntity.getTitle();
+	}
+}
